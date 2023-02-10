20205170d1f357240c6daaf847561d94bfb1fb44
cdgrody <cdgrody@gmail.com>
2023-02-09 20:50:46 -0800

    Many-to-many relationship between movies and stones is formed.

--- main_app/models.py
+++ main_app/models.py
@@ -5,36 +5,6 @@ STONE_COLORS = (('R', 'Red'), ('P', 'Purple'), ('Y', 'Yellow'), ('O', 'Orange'),
 IS_HERO = ((True, 'Yes'), (False, 'No'),)
 MOVIE_TYPE = ((True, 'Ensemble'), (False, 'Solo'),)
 
-# Create your models here.
-class Stone(models.Model):
-    name = models.CharField(max_length=100)
-    numberOfAppearances = models.PositiveSmallIntegerField(default=0)
-    description = models.TextField()
-    color = models.CharField(
-        max_length=100, 
-        choices=STONE_COLORS,
-        default=STONE_COLORS[0][1]
-    )
-
-    def __str__(self):
-        return f'{self.name}'
-
-    def get_absolute_url(self):
-        return reverse('detail', kwargs={'stone_id': self.id})
-
-class Weilder(models.Model):
-    character = models.CharField(max_length=100)
-    is_hero = models.BooleanField(choices=IS_HERO)
-    phase_of_use = models.PositiveSmallIntegerField()
-    stone = models.ForeignKey(Stone, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.character}'
-
-    class Meta:
-        ordering = ['-phase_of_use', 'character', 'is_hero']
-
-
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     is_ensemble = models.BooleanField( 'Ensemble or Solo Film', choices=MOVIE_TYPE, default=True)
@@ -48,3 +18,35 @@ class Movie(models.Model):
 
     class Meta:
         ordering = ['-release_date']
+
+
+# Create your models here.
+class Stone(models.Model):
+    name = models.CharField(max_length=100)
+    numberOfAppearances = models.PositiveSmallIntegerField(default=0)
+    description = models.TextField()
+    movies = models.ManyToManyField(Movie)
+    color = models.CharField(
+        max_length=100, 
+        choices=STONE_COLORS,
+        default=STONE_COLORS[0][1]
+    )
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'stone_id': self.id})
+
+
+class Weilder(models.Model):
+    character = models.CharField(max_length=100)
+    is_hero = models.BooleanField(choices=IS_HERO)
+    phase_of_use = models.PositiveSmallIntegerField()
+    stone = models.ForeignKey(Stone, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.character}'
+
+    class Meta:
+        ordering = ['-phase_of_use', 'character', 'is_hero']
